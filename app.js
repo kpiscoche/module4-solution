c891250dedb65185a0c30a1544acf6c1e268eb80
kpiscoche <53128160+kpiscoche@users.noreply.github.com>
2019-09-08 22:06:19 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+input = 'l'
+
+if(input=='J' || input=='j'){
+	console.log("Good Bye");
+}else{
+	console.log("Hello");
+}
+
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
